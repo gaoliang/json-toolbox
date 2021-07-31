e115a3d520b744640532c84d88e3d26584b1467e
GitHub Action <action@github.com>
2021-07-31 04:26:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/gaoliang/jsontoolbox/MyBundle.kt
+++ src/main/kotlin/com/github/gaoliang/jsontoolbox/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gaoliang.jsontoolbox
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gaoliang/jsontoolbox/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gaoliang/jsontoolbox/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gaoliang.jsontoolbox.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gaoliang.jsontoolbox.MyBundle
 
 class MyApplicationService {
 
